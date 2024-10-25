9351505aaa8d4752dbaf3adab02cce184d09f3b4
AchrefAmri <achref.amri@capgemini.com>
2024-10-25 14:30:01 +0100

    deploy glue on dev

---
+++ test_glue/variables.tf
@@ -0,0 +1,14 @@
+
+variable "glue_job_name" {
+  description = "The name of the Glue job"
+  default     = "test_my_glue"
+}
+
+
+variable "scriptpath" {
+  description = "The name of the Glue job"
+  default     = "s3://my-bucket/achreftesting/test_glue.py"
+}
+
+
+
